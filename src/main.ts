609d8da86c3a7e09bbb64d81c38e8e00d8c49a1e
naruesantos <naruesantos@gmail.com>
2025-02-19 15:28:22 -0300

    :wrench:chore: configure cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://your-frontend-domain.com', // Allow only specific origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    credentials: true, // Allow cookies (optional)
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+  
 }
 bootstrap();
