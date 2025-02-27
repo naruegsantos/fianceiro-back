3643e115881a72b50af643ae4760b607d5b65e29
Naruê Gonçalves Santos <naruegsantos@gmail.com>
2025-02-27 11:26:44 -0300

    hotfix: try to fix production bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [UserModule, ConfigModule.forRoot({
     isGlobal: true,
-  }), AccountModule, RecordModule, PrismaModule],
+  }), AccountModule, RecordModule],
   controllers: [AppController],
   providers: [AppService],
 })
