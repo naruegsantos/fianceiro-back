269a6d851a7a47ea21e883a6be57a7f0328ce967
Naruê Gonçalves Santos <naruegsantos@gmail.com>
2025-02-28 10:38:27 -0300

    chore: configure open cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors:true});
 
-  app.enableCors({
-    origin: '*', // Allow only specific origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-    allowedHeaders: 'Content-Type, Accept', // Allowed headers
-    credentials: true, // Allow cookies (optional)
-  });
 
   await app.listen(process.env.PORT ?? 3001);
   
