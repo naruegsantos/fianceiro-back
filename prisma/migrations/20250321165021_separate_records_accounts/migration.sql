16e1ced5677f9b2572377fe7b4f32ca403a72c5d
Narue G. Santos <narue.santos@24hub.com>
2025-04-08 13:50:00 -0300

    chore: update migrations

---
+++ prisma/migrations/20250321165021_separate_records_accounts/migration.sql
@@ -0,0 +1,55 @@
+-- CreateEnum
+CREATE TYPE "recordCategories" AS ENUM ('REVENUE', 'EXPENSE', 'TRANSFERENCE');
+
+-- CreateEnum
+CREATE TYPE "accountCategories" AS ENUM ('REVENUE', 'EXPENSE', 'ASSET');
+
+-- CreateEnum
+CREATE TYPE "roles" AS ENUM ('ADMIN', 'OPERATOR', 'GUEST');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" SERIAL NOT NULL,
+    "label" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "accountCategory" "accountCategories" NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Record" (
+    "id" SERIAL NOT NULL,
+    "label" TEXT NOT NULL,
+    "cameFromId" INTEGER NOT NULL,
+    "wentToId" INTEGER NOT NULL,
+    "recordCategory" "recordCategories" NOT NULL,
+    "date" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "description" TEXT,
+
+    CONSTRAINT "Record_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "roles" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Record" ADD CONSTRAINT "Record_cameFromId_fkey" FOREIGN KEY ("cameFromId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Record" ADD CONSTRAINT "Record_wentToId_fkey" FOREIGN KEY ("wentToId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
